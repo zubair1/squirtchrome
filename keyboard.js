9bbb4a44d92f4efbdf9f1c6712bcc21283ea7058
Zubair <zubair11@hotmail.com>
2017-12-26 03:34:29 +0500

    Changed keyboard shortcut to CTRL+Z

--- keyboard.js
+++ keyboard.js
@@ -1,6 +1,6 @@
 // in the content script, listen for Crtl+Shift+E (upper or lowercase)
 document.documentElement.addEventListener("keypress", function(e) {
-    if( e.keyCode == 5 || ((e.keyCode == 69 || e.keyCode == 101) && e.ctrlKey && e.shiftKey) ) {
+    if( e.keyCode == 26 || ((e.keyCode == 90 || e.keyCode == 122) && e.ctrlKey && e.shiftKey) ) {
 		var extpath = extpath = chrome.extension.getURL('');
 		var action_url = "javascript:(function(){if(window.sq){ window.sq.closed && window.document.dispatchEvent(new Event('squirt.again')); } else { window.sq = {}; window.sq.extpath = '"+extpath+"'; window.sq.userId = '--squirtUser--'; s = document.createElement('script'); s.src = '"+chrome.extension.getURL('squirt.js')+"'; s.s = window.location.search; s.idx = s.s.indexOf('sq-dev'); document.body.appendChild(s); }})();";
 
